9aa3cab8703590d2931c89c258ef1f8609ccdf26
Prafull Gururani <praguru14@gmail.com>
2024-01-23 12:19:48 +0530

    Adding a few services

---
+++ config-ser/config-ser/src/test/java/com/microservices/configser/ConfigSerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.configser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ dept-ser/dept-ser/src/test/java/com/microservices/deptser/DeptSerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.deptser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeptSerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
